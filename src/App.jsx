a693c05448422d09b2cdbd1b7debbc1dfec70caa
jaim13 <jaimmartinez13@gmail.com>
2024-05-31 18:51:26 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Stopwatch from './components/Stopwatch'
+function App() {
+  return (
+    <div className='content'>
+      <h1>
+        Welcome Let's Practice React
+      </h1>
+      <Stopwatch />
+    </div>
+  )
+}
+
+export default App
